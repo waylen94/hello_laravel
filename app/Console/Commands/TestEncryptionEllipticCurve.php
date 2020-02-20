b9ec405d51401d9deb83a699fb97492b6536865c
vagrant <vagrant@homestead>
2020-02-20 13:41:49 +0000

    dealing with decrypted message corresponded data

--- app/Console/Commands/TestEncryptionEllipticCurve.php
+++ app/Console/Commands/TestEncryptionEllipticCurve.php
@@ -8,6 +8,17 @@ use Illuminate\Console\Command;
 use FG\ASN1\Universal\Sequence;
 use FG\ASN1\Universal\ObjectIdentifier;
 use FG\ASN1\Universal\BitString;
+//obtain needed ECC Library
+use Mdanter\Ecc\EccFactory;
+use Mdanter\Ecc\Primitives\Point;
+use Mdanter\Ecc\Primitives\PointInterface;
+use Mdanter\Ecc\Random\RandomGeneratorFactory;
+use Mdanter\Ecc\Serializer\Point\CompressedPointSerializer;
+use Mdanter\Ecc\Serializer\Util\CurveOidMapper;
+use Mdanter\Ecc\Serializer\PrivateKey\PemPrivateKeySerializer;
+use Mdanter\Ecc\Serializer\PrivateKey\DerPrivateKeySerializer;
+use Mdanter\Ecc\Serializer\PublicKey\DerPublicKeySerializer;
+use Mdanter\Ecc\Serializer\PublicKey\PemPublicKeySerializer;
 
 
 class TestEncryptionEllipticCurve extends Command
@@ -48,34 +59,96 @@ class TestEncryptionEllipticCurve extends Command
         //runnign the result through a KDF,
         // and then using that as a symmetric key to actually encrypt that data.
 
-        
-       $private_key_simulation = "";
-       $public_key_simulation = "";
+        //corresponded public and private key retrieved from Google Tink Library
+       $private_key_simulation = "MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgCPSuFr4iSIaQprjj"
+       + "chHPyDu2NXFe0vDBoTpPkYaK9dehRANCAATnaFz/vQKuO90pxsINyVNWojabHfbx"
+       + "9qIJ6uD7Q7ZSxmtyo/Ez3/o2kDT8g0pIdyVIYktCsq65VoQIDWSh2Bdm";
 
-       $ephemeral_public_key_signedmessage_received = "";
+       $public_key_simulation = "BOdoXP+9Aq473SnGwg3JU1aiNpsd9vH2ognq4PtDtlLGa3Kj8TPf+jaQNPyDSkh3JUhiS0KyrrlWhAgNZKHYF2Y=";
 
-       $standard_decrypted_message = "";
+       $Google_provided_public_Key = "{\n"
+        + "  \"keys\": [\n"
+        + "    {\n"
+        + "      \"keyValue\": \"MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEPYnHwS8uegWAewQtlxizmLFynw"
+        + "HcxRT1PK07cDA6/C4sXrVI1SzZCUx8U8S0LjMrT6ird/VW7be3Mz6t/srtRQ==\",\n"
+        + "      \"protocolVersion\": \"ECv1\"\n"
+        + "    },\n"
+        + "    {\n"
+        + "      \"keyValue\": \"MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE/1+3HBVSbdv+j7NaArdgMyoSAM"
+        + "43yRydzqdg1TxodSzA96Dj4Mc1EiKroxxunavVIvdxGnJeFViTzFvzFRxyCw==\",\n"
+        + "      \"keyExpiration\": \""
+        + Instant.now().plus(Duration.standardDays(1)).getMillis()
+        + "\",\n"
+        + "      \"protocolVersion\": \"ECv2\"\n"
+        + "    },\n"
+        + "    {\n"
+        + "      \"keyValue\": \"MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAENXvYqxD5WayKYhuXQevdGdLA8i"
+        + "fV4LsRS2uKvFo8wwyiwgQHB9DiKzG6T/P1Fu9Bl7zWy/se5Dy4wk1mJoPuxg==\",\n"
+        + "      \"keyExpiration\": \""
+        + Instant.now().plus(Duration.standardDays(1)).getMillis()
+        + "\",\n"
+        + "      \"protocolVersion\": \"ECv2SigningOnly\"\n"
+        + "    },\n"
+        + "  ],\n"
+        + "}";
 
+    $GOOGLE_SIGNING_EC_V2_INTERMEDIATE_PUBLIC_KEY_X509_BASE64 = "";
+
+    $ephemeral_public_key_signedmessage_received = "";
+
+    $standard_decrypted_message = "plaintext";
+
+    $RECIPIENT_ID = "someRecipient";
+
+    $signed_message = "{"
+        + "\"protocolVersion\":\"ECv1\","
+        + "\"signedMessage\":"
+        + ("\"{"
+            + "\\\"tag\\\":\\\"ZVwlJt7dU8Plk0+r8rPF8DmPTvDiOA1UAoNjDV+SqDE\\\\u003d\\\","
+            + "\\\"ephemeralPublicKey\\\":\\\"BPhVspn70Zj2Kkgu9t8+ApEuUWsI/zos5whGCQBlgOkuYagOis7"
+            + "qsrcbQrcprjvTZO3XOU+Qbcc28FSgsRtcgQE\\\\u003d\\\","
+            + "\\\"encryptedMessage\\\":\\\"12jUObueVTdy\\\"}\",")
+        + "\"signature\":\"MEQCIDxBoUCoFRGReLdZ/cABlSSRIKoOEFoU3e27c14vMZtfAiBtX3pGMEpnw6mSAbnagC"
+        + "CgHlCk3NcFwWYEyxIE6KGZVA\\u003d\\u003d\"}";
 
 
         echo "Beigining Testing ECC Algorithm".PHP_EOL.PHP_EOL;
+        
+        //1. receive all needed resource for decryption -1. ephemeral_publick_key; -2. private_key -3. encrypted_message 
+        $signed_message_array = json_decode($signed_message,true);
+        $ephemeral_public_key = $signed_message_array['signedMessage']['ephemeralPublickey'];
+        $private_key = $private_key_simulation;
+        $encrypted_message = $signed_message_array['signedMesage']['encyptedMessage'];
 
-        $ephemeralPublickey_uncompressed_raw = "BOdoXP+9Aq473SnGwg3JU1aiNpsd9vH2ognq4PtDtlLGa3Kj8TPf+jaQNPyDSkh3JUhiS0KyrrlWhAgNZKHYF2Y=";
 
-        $ephemeral_point = $this->key_unserialize(bin2hex(base64_decode($ephemeralPublickey_uncompressed_raw)));
 
-        var_dump( $ephemeral_point) ;
 
-        $ephemeralPublickey_uncompressed = new PublicKey($ephemeral_point);
 
-        $ephemeralPublicKey = $this->publickey_serialize($ephemeralPublickey_uncompressed);
-        //resource version one
-        $this->resource_version1();
+
+
+
+
+
+
+
+
+        // $ephemeralPublickey_uncompressed_raw = "BOdoXP+9Aq473SnGwg3JU1aiNpsd9vH2ognq4PtDtlLGa3Kj8TPf+jaQNPyDSkh3JUhiS0KyrrlWhAgNZKHYF2Y=";
+
+        // $ephemeral_point = $this->key_unserialize(bin2hex(base64_decode($ephemeralPublickey_uncompressed_raw)));
+
+        // var_dump( $ephemeral_point) ;
+
+        // $ephemeralPublickey_uncompressed = new PublicKey($ephemeral_point);
+
+        // $ephemeralPublicKey = $this->publickey_serialize($ephemeralPublickey_uncompressed);
+        // //resource version one
+        // $this->resource_version1();
 
 
         echo $ephemeralPublicKey.PHP_EOL.PHP_EOL;
-
     }
+
+
     public function resource_version1(){
         //Resource version 1 
         $form_data_str = 'random string generated for testing';
@@ -90,212 +163,6 @@ class TestEncryptionEllipticCurve extends Command
         echo "<br>Decrypted: ".$decrypted_data.PHP_EOL;
         //resource version 1 finished
     }
-    /**
-     * @param string           $data
-     * @return Point
-     */
-    public function key_unserialize(string $data): Point
-    {
-        if ($this->substring($data, 0, 2) != '04') {
-            return false;
-        }
-
-        $data = $this->substring($data, 2);
-        $dataLength = $this->length($data);
-
-        $x = gmp_init($this->substring($data, 0, $dataLength / 2), 16);
-        $y = gmp_init($this->substring($data, $dataLength / 2), 16);
-
-        return new Point($x,$y);
-    }
-
-/**
-     * Multi-byte-safe substring calculation
-     *
-     * @param string $str
-     * @param int $start
-     * @param int $length (optional)
-     * @return string
-     */
-    public function substring(string $str, int $start = 0, int $length = null): string
-    {
-        // Premature optimization: cache the function_exists() result
-        static $exists = null;
-        if ($exists === null) {
-            $exists = function_exists('mb_substr');
-        }
-
-        // If it exists, we need to make sure we're using 8bit mode
-        if ($exists) {
-            return mb_substr($str, $start, $length, '8bit');
-        } elseif ($length !== null) {
-            return substr($str, $start, $length);
-        }
-        return substr($str, $start);
-    }
-        /**
-     * Multi-byte-safe string length calculation
-     *
-     * @param string $str
-     * @return int
-     */
-    public function length(string $str): int
-    {
-        // Premature optimization: cache the function_exists() result
-        static $exists = null;
-        if ($exists === null) {
-            $exists = function_exists('mb_strlen');
-        }
-
-        // If it exists, we need to make sure we're using 8bit mode
-        if ($exists) {
-            return mb_strlen($str, '8bit');
-        }
-        return strlen($str);
-    }
-
-    /**
-     * specificied for uncompressed public key
-     * {@inheritDoc}
-     * @see \Mdanter\Ecc\Serializer\PublicKey\PublicKeySerializerInterface::serialize()
-     */
-    public function publickey_serialize(PublicKey $key): string
-    {
-        $publicKeyInfo = $this->format($key);
-
-        $content  = '-----BEGIN PUBLIC KEY-----'.PHP_EOL;
-        $content .= trim(chunk_split(base64_encode($publicKeyInfo), 64, PHP_EOL)).PHP_EOL;
-        $content .= '-----END PUBLIC KEY-----';
-
-        return $content;
-    }
-    //1.2.840.10045.2.1
-    //ANSI X9.62 standard (1998) "Public Key Cryptography for the Financial Services Industry: The Elliptic Curve Digital Signature Algorithm (ECDSA)"
-   //1.2.840.10045.3.1.7
-   //256-bit Elliptic Curve Cryptography (ECC), also known as National Institute of Standards and Technology (NIST) P-256
-
-    public function format(PublicKey $key): string
-    {
-        $sequence = new Sequence(
-            new Sequence(
-                new ObjectIdentifier('1.2.840.10045.2.1'), 
-                new ObjectIdentifier('1.2.840.10045.3.1.7')
-            ),
-            new BitString($this->point_serialize($key->getPoint()))
-        );
-        return $sequence->getBinary();
-    }
-
-   /**
-     * @param PointInterface $point
-     * @return string
-     */
-    public function point_serialize(Point $point): string
-    {
-        $length = 32 * 2;
-
-        $hexString = '04';
-        $hexString .= str_pad(gmp_strval($point->getX(), 16), $length, '0', STR_PAD_LEFT);
-        $hexString .= str_pad(gmp_strval($point->getY(), 16), $length, '0', STR_PAD_LEFT);
-
-        return $hexString;
-    }
-
-
 }
-
-
-
-
-class Point{
-    /**
-     * @var \GMP
-     */
-    private $x;
-
-    /**
-     * @var \GMP
-     */
-    private $y;
-
-
-    /**
-     * Initialize a new instance
-     *
-     * @param \GMP                 $x
-     * @param \GMP                 $y
-     *
-     */
-    public function __construct( \GMP $x, \GMP $y)
-    {
-        // $this->curve      = $curve;
-        $this->x          = $x;
-        $this->y          = $y;
-    }
-
-  
-    /**
-     * {@inheritDoc}
-     * @see \Mdanter\Ecc\Primitives\PointInterface::getX()
-     */
-    public function getX(): \GMP
-    {
-        return $this->x;
-    }
-
-    /**
-     * {@inheritDoc}
-     * @see \Mdanter\Ecc\Primitives\PointInterface::getY()
-     */
-    public function getY(): \GMP
-    {
-        return $this->y;
-    }
-
-}
-
-class PublicKey{
-    /**
-     *
-     * @var PointInterface
-     */
-    private $point;
-    /**
-     * Initialize a new PublicKey instance.
-     *
-     * @param  GmpMathInterface  $adapter
-     * @param  PointInterface    $point
-     */
-    public function __construct($point)
-    {
-        $this->point = $point;
-        // $this->adapter = $adapter;
-
-
-        // //TO DO security issue partial public key validation routine
-        // if ($adapter->cmp($point->getX(), gmp_init(0, 10)) < 0 || $adapter->cmp($this->curve->getPrime(), $point->getX()) < 0
-        //     || $adapter->cmp($point->getY(), gmp_init(0, 10)) < 0 || $adapter->cmp($this->curve->getPrime(), $point->getY()) < 0
-        // ) {
-        //     throw new PublicKeyException($generator, $point, "Point has x and y out of range.");
-        // }
-
-        // Sanity check. Point (x,y) values are qualified against it's
-        // generator and curve. Here we ensure the Point and Generator
-        // are the same.
-        // if (!$generator->getCurve()->equals($point->getCurve())) {
-        //     throw new PublicKeyException($generator, $point, "Curve for given point not in common with GeneratorPoint");
-        // }
-    }
-    /**
-     * {@inheritDoc}
-     * @see \Mdanter\Ecc\Crypto\Key\PublicKeyInterface::getPoint()
-     */
-    public function getPoint(): Point
-    {
-        return $this->point;
-    }
-
-}
-
 
 
